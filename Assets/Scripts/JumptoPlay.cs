e130e847f50c669a868dabf10b251f78c4d1954c
CYL5266 <1378576995@qq.com>
2024-10-24 13:50:44 +0800

    VR手柄实现

--- Assets/Scripts/JumptoPlay.cs
+++ Assets/Scripts/JumptoPlay.cs
@@ -2,42 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class JumptoPlay : MonoBehaviour
 {
-    public Slider progressBar; // 进度条引用
-
-    // 方法调用以加载新场景
-    public void LoadScene(string sceneName)
+    // Start is called before the first frame update
+    void Start()
     {
-        StartCoroutine(LoadSceneAsync(sceneName));
+        
     }
 
-
-    // 异步加载场景
-    private IEnumerator LoadSceneAsync(string sceneName)
+    // Update is called once per frame
+    void Update()
     {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
-        operation.allowSceneActivation = false; // 防止场景自动激活
+        
+    }
 
-        // 当场景未加载完成时
-        while (!operation.isDone)
-        {
-            // 更新进度条值
-            // 注意：operation.progress 范围是 0 到 0.9
-            float progress = Mathf.Clamp01(operation.progress / 0.9f);
-            progressBar.value = progress;
-
-            // 当进度达到 0.9，允许场景激活
-            if (operation.progress >= 0.9f)
-            {
-                // 等待一段时间后再激活场景
-                //yield return new WaitForSeconds(1f); // 可自定义等待时间
-                operation.allowSceneActivation = true; // 激活场景
-            }
-
-            yield return null; // 等待下一帧
-        }
+    public void jump()
+    {
+        SceneManager.LoadScene(1);
     }
 }
